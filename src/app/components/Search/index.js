eb8def9b99364147631dee1a0dcb895722a481fb
joykomaling <joyfernin11@gmail.com>
2024-01-30 13:17:37 +0700

    first commit

---
+++ src/app/components/Search/index.js
@@ -0,0 +1,11 @@
+const Search = () => {
+    return (
+        <div className="grid grid-cols-3 bg-indigo-500">
+            <div>KOTAK 1</div>
+            <div>KOTAK 2</div>
+            <div>KOTAK 3</div>
+        </div>
+    )
+}
+
+export default Search
